fca49cf9a9a3156ee4c404c801a8ab9e62b12be9
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-11-07 13:12:27 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import React, { useRef, useEffect, useState } from 'react'
+
+const videoFormats = [
+  'mp4',
+  'avi',
+  'mov',
+  'mpg',
+  'wmv',
+  'flv',
+  'webm',
+  'mkv',
+  'ogv',
+  '3gp',
+  '3g2'
+]
+const audioFormats = ['mp3', 'wav', 'flac', 'ogg']
 
 function App() {
+  const fileInput = useRef()
+  const [tmpPath, setTmpPath] = useState('')
+  const [isVideo, setIsVideo] = useState(true)
+  const [notSupported, setNotSuported] = useState(false)
+  const [extension, setExt] = useState('')
+
+  const selectImage = () => {
+    if (fileInput.current) {
+      document.getElementById('filepicker').click()
+    }
+  }
+  const handleImageSelected = e => {
+    try {
+      const { files } = e.target
+      const firstFile = files[0]
+      const { name } = firstFile
+      const extArray = name.split('.')
+      const ext = extArray[extArray.length - 1]
+      setExt(ext)
+      const isVideoFile = videoFormats.includes(ext)
+      const isAudioFile = audioFormats.includes(ext)
+      if (isVideoFile) {
+        setIsVideo(true)
+      } else if (isAudioFile) {
+        setIsVideo(false)
+      } else {
+        setNotSuported(true)
+      }
+      const temp = URL.createObjectURL(firstFile)
+      setTmpPath(() => temp)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  useEffect(() => {
+    if (tmpPath && isVideo) {
+      const src = document.getElementById('videoSource')
+      src.setAttribute('src', tmpPath)
+    } else if (tmpPath && !isVideo && !notSupported) {
+      const src = document.getElementById('audioSource')
+      src.setAttribute('src', tmpPath)
+    }
+  }, [tmpPath, isVideo, notSupported])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='video-container'>
+        {tmpPath && isVideo && (
+          <video className='video' controls>
+            <source
+              id='videoSource'
+              src={tmpPath}
+              type={`video/${extension}`}
+            />
+            Your browser does not support the video tag.
+          </video>
+        )}
+        {tmpPath && !isVideo && !notSupported && (
+          <audio className='audio' controls>
+            <source
+              id='audioSource'
+              src={tmpPath}
+              type={`audio/${extension}`}
+            />
+            Your browser does not support the video tag.
+          </audio>
+        )}
+        {tmpPath && !isVideo && notSupported && (
+          <h1> Sorry this video format is not supported ...</h1>
+        )}
+      </div>
+      <div className='button-row'>
+        <button onClick={selectImage}> Select Image...</button>
+        <span>{tmpPath}</span>
+        <input
+          type='file'
+          accept='video/mp4,video/x-m4v,video/*,audio/*'
+          ref={fileInput}
+          onChange={handleImageSelected}
+          id='filepicker'
+          name='select'
+          className='hidden'
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
